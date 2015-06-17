d7a0a142e21e68a3d06795687cde7a7e7bd8a7a6
Ryan Graham <r.m.graham@gmail.com>
2015-06-16 22:30:43 -0700

    Initial implementation
    More or less on par with mktmpio/node-mktmpio

--- mktmpio.go
+++ mktmpio.go
@@ -1,17 +1,41 @@
 package main
 
 import (
-  "os"
-  "github.com/codegangsta/cli"
+	"fmt"
+	"github.com/codegangsta/cli"
+	"github.com/mktmpio/go-mktmpio/mktmpio"
+	"os"
+	"time"
 )
 
 func main() {
-  app := cli.NewApp()
-  app.Name = "mktmpio"
-  app.Usage = "create, destroy, and manage mktmpio instances"
-  app.Action = func(c *cli.Context) {
-    println("Coming soon!")
-  }
+	app := cli.NewApp()
+	app.Name = "mktmpio"
+	app.Usage = "create, destroy, and manage mktmpio instances"
+	app.Action = func(c *cli.Context) {
+		err, client := mktmpio.NewClient()
+		err, instance := client.Create(c.Args()[0])
+		if err != nil {
+			fmt.Printf("Error creating instance: %s\n", err)
+			return
+		}
+		defer func() {
+			instance.Destroy()
+			fmt.Printf("Instance %s terminated.\n", instance.Id)
+		}()
+		_ = instance.LoadEnv()
+		cmd := instance.Cmd()
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		// MySQL is particularly slow to start up
+		if instance.Type == "mysql" {
+			time.Sleep(500 * time.Millisecond)
+		} else {
+			time.Sleep(100 * time.Millisecond)
+		}
+		err = cmd.Run()
+	}
 
-  app.Run(os.Args)
+	app.Run(os.Args)
 }
