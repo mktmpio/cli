ab21b66f43f634b4811099e019d0f9dc44ec6c16
Ryan Graham <r.m.graham@gmail.com>
2015-06-27 20:20:33 -0700

    use a remote shell when available

--- mktmpio.go
+++ mktmpio.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/codegangsta/cli"
 	"github.com/mktmpio/go-mktmpio"
+	"golang.org/x/crypto/ssh/terminal"
+	"io"
 	"os"
 	"time"
 )
@@ -34,19 +36,62 @@ func main() {
 				fmt.Printf("Instance %s terminated.\n", instance.ID)
 			}
 		}()
-		_ = instance.LoadEnv()
-		cmd := instance.Cmd()
-		cmd.Stdin = os.Stdin
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		// MySQL is particularly slow to start up
-		if instance.Type == "mysql" {
-			time.Sleep(500 * time.Millisecond)
+		if len(instance.ContainerShell) > 0 {
+			if err = remoteShell(client, instance); err != nil {
+				fmt.Printf("Error running remote shell %s: %v\n", instance.ID, err)
+			}
 		} else {
-			time.Sleep(100 * time.Millisecond)
+			localShell(instance)
 		}
-		err = cmd.Run()
 	}
 
 	app.Run(os.Args)
+}
+
+func localShell(instance *mktmpio.Instance) error {
+	_ = instance.LoadEnv()
+	cmd := instance.Cmd()
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	// MySQL is particularly slow to start up
+	if instance.Type == "mysql" {
+		time.Sleep(500 * time.Millisecond)
+	} else {
+		time.Sleep(100 * time.Millisecond)
+	}
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Error running local shell for %s: %v\n", instance.ID, err)
+		return err
+	}
+	return nil
+}
+
+func remoteShell(client *mktmpio.Client, instance *mktmpio.Instance) error {
+	reader, writer, err := client.Attach(instance.ID)
+	errs := make(chan error)
+	pipe := func(r io.Reader, w io.Writer) {
+		buf := make([]byte, 128)
+		for {
+			n, err := r.Read(buf)
+			if n > 0 {
+				w.Write(buf[:n])
+			}
+			if err != nil {
+				errs <- err
+				return
+			}
+		}
+	}
+	if err != nil {
+		return err
+	}
+	oldState, err := terminal.MakeRaw(0)
+	if err != nil {
+		panic(err)
+	}
+	defer terminal.Restore(0, oldState)
+	go pipe(os.Stdin, writer)
+	go pipe(reader, os.Stdout)
+	return <-errs
 }
